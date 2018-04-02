2a29fe52019e9b3b17caff2c212789bcd4ac4f80
Sean Kennedy <sean@kennedy.software>
2018-04-01 23:15:52 -0400

    Added failing test stubs for examples

---
+++ time-range/main_test.go
@@ -0,0 +1,30 @@
+package time_range
+
+import "testing"
+
+
+// Test given examples
+func TestMinusPartialIntersection(t *testing.T) {
+
+	t.Errorf("Not implemented")
+}
+
+func TestMinusSameRange(t *testing.T) {
+
+	t.Errorf("Not implemented")
+}
+
+func TestMinusNonIntersectingRange(t *testing.T) {
+
+	t.Errorf("Not implemented")
+}
+
+func TestManyRangesMinusOneRange(t *testing.T) {
+
+	t.Errorf("Not implemented")
+}
+
+func TestManyRangesMinusManyRanges(t *testing.T) {
+
+	t.Errorf("Not implemented")
+}
