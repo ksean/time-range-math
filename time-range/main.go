a54ece3a40c40c07e8f996bf0657100596c12043
Sean Kennedy <skennedy@hashtagpaid.com>
2018-04-04 15:04:00 -0400

    Implemented first failing test for partial intersection for subtracting 1 time range from 1 other range, added EditorConfig

--- time-range/main.go
+++ time-range/main.go
@@ -1,1 +1,16 @@
 package time_range
+import "time"
+
+
+type Timerange struct {
+    start time.Time
+    end time.Time
+}
+
+func SubtractTimeranges(minuend []Timerange, subtrahend []Timerange) []Timerange {
+	var result []Timerange
+
+	// TODO: Implement function
+
+	return result
+}
--- time-range/main_test.go
+++ time-range/main_test.go
@@ -1,12 +1,48 @@
 package time_range
 
-import "testing"
+import (
+	"testing"
+	"time"
+	"reflect"
+	"bytes"
+	"fmt"
+	"strings"
+)
 
+
+var today = time.Now()
 
 // Test given examples
 func TestMinusPartialIntersection(t *testing.T) {
 
-	t.Errorf("Not implemented")
+	range1Start := getTimeFromTodayUsingHoursMinutesSeconds(9, 0, 0)
+	range1End := getTimeFromTodayUsingHoursMinutesSeconds(12, 0, 0)
+	range2Start := getTimeFromTodayUsingHoursMinutesSeconds(11, 30, 0)
+	range2End := getTimeFromTodayUsingHoursMinutesSeconds(14, 0, 0)
+
+	minuendRange := Timerange{range1Start, range1End}
+	subtrahendRange := Timerange{range2Start, range2End}
+
+	var minuendRanges []Timerange
+	var subtrahendRanges []Timerange
+
+	minuendRanges = append(minuendRanges, minuendRange)
+	subtrahendRanges = append(subtrahendRanges, subtrahendRange)
+
+	result := SubtractTimeranges(minuendRanges, subtrahendRanges)
+
+	expectedRangeStart := getTimeFromTodayUsingHoursMinutesSeconds(9, 0, 0)
+	expectedRangeEnd := getTimeFromTodayUsingHoursMinutesSeconds(11, 30, 0)
+
+	expectedRange := Timerange{expectedRangeStart, expectedRangeEnd}
+
+	var expectedRanges []Timerange
+
+	expectedRanges = append(expectedRanges, expectedRange)
+
+	if !reflect.DeepEqual(expectedRanges, result) {
+		t.Errorf("Expected: %s, Actual: %s", timerangesToString(expectedRanges), timerangesToString(result))
+	}
 }
 
 func TestMinusSameRange(t *testing.T) {
@@ -27,4 +63,18 @@ func TestManyRangesMinusOneRange(t *testing.T) {
 func TestManyRangesMinusManyRanges(t *testing.T) {
 
 	t.Errorf("Not implemented")
+}
+
+func getTimeFromTodayUsingHoursMinutesSeconds(hours int, minutes int, seconds int) time.Time {
+	return time.Date(today.Year(), today.Month(), today.Day(), hours, minutes, seconds, 0, time.UTC)
+}
+
+func timerangesToString(timeranges []Timerange) string {
+	var buffer bytes.Buffer
+
+	for _, v := range timeranges {
+		buffer.WriteString(fmt.Sprintf("(start: %s, end: %s),", v.start.String(), v.end.String()))
+	}
+
+	return strings.TrimRight(buffer.String(), ",")
 }
