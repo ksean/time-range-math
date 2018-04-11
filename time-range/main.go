b102b385f3d635d678a8a6778eab96044f7d18ba
Sean Kennedy <skennedy@hashtagpaid.com>
2018-04-11 17:01:31 -0400

    Implemented remaining test stubs, implemented basic subtraction solution (using iterated 'in' checks), all tests now passing except for many minus many test

--- time-range/main.go
+++ time-range/main.go
@@ -10,7 +10,81 @@ type Timerange struct {
 func SubtractTimeranges(minuend []Timerange, subtrahend []Timerange) []Timerange {
 	var result []Timerange
 
-	// TODO: Implement function
+	result = simpleSubtractManyFromManyTimeranges(minuend, subtrahend)
 
 	return result
+}
+
+func simpleSubtractManyFromManyTimeranges(minuend []Timerange, subtrahend []Timerange) []Timerange {
+	var result = minuend
+
+	for _, timerange := range subtrahend {
+		result = simpleSubtractOneFromManyTimeranges(result, timerange)
+	}
+
+	return result
+}
+
+func simpleSubtractOneFromManyTimeranges(minuend []Timerange, subtrahend Timerange) []Timerange {
+	var result []Timerange
+
+	for _, timerange := range minuend {
+		trimmed := simpleSubtractOneFromOneTimerange(timerange, subtrahend)
+		result = append(result, trimmed...)
+	}
+
+	return result
+}
+
+func simpleSubtractOneFromOneTimerange(minuend Timerange, subtrahend Timerange) []Timerange {
+	var result []Timerange
+
+	subtrahendStartInRange := timeInRange(minuend, subtrahend.start)
+	subtrahendEndInRange := timeInRange(minuend, subtrahend.end)
+
+	// Return empty slice conditions:
+	// Exact time range match
+	// Subtrahend is a superset of minuend
+	if (minuend.start == subtrahend.start && minuend.end == subtrahend.end) ||
+		(!subtrahendStartInRange && !subtrahendEndInRange &&
+		subtrahend.start.Before(minuend.start) && subtrahend.end.After(minuend.end)) {
+
+		return result
+	}
+
+	// 4 cases:
+	// 1 No intersection
+	if !subtrahendStartInRange && !subtrahendEndInRange {
+		result = append(result, minuend)
+
+	// 2 Partial intersection @ end
+	} else if subtrahendStartInRange && !subtrahendEndInRange {
+		minuend.end = subtrahend.start
+		result = append(result, minuend)
+
+	// 3 Partial intersection @ start
+	} else if !subtrahendStartInRange && subtrahendEndInRange {
+		minuend.start = subtrahend.end
+		result = append(result, minuend)
+
+	// 4 Minuend UNION subtrahend == Minuend
+	} else {
+		var firstBisection Timerange
+		var secondBisection Timerange
+
+		firstBisection.start = minuend.start
+		firstBisection.end = subtrahend.start
+
+		secondBisection.start = subtrahend.end
+		secondBisection.end = minuend.start
+
+		result = append(result, firstBisection)
+		result = append(result, secondBisection)
+	}
+
+	return result
+}
+
+func timeInRange(timerange Timerange, time time.Time) bool {
+	return timerange.start.Before(time) && timerange.end.After(time)
 }
--- time-range/main_test.go
+++ time-range/main_test.go
@@ -16,9 +16,9 @@ var today = time.Now()
 func TestMinusPartialIntersection(t *testing.T) {
 
 	range1Start := getTimeFromTodayUsingHoursMinutesSeconds(9, 0, 0)
-	range1End := getTimeFromTodayUsingHoursMinutesSeconds(12, 0, 0)
-	range2Start := getTimeFromTodayUsingHoursMinutesSeconds(11, 30, 0)
-	range2End := getTimeFromTodayUsingHoursMinutesSeconds(14, 0, 0)
+	range1End := getTimeFromTodayUsingHoursMinutesSeconds(10, 0, 0)
+	range2Start := getTimeFromTodayUsingHoursMinutesSeconds(9, 0, 0)
+	range2End := getTimeFromTodayUsingHoursMinutesSeconds(9, 30, 0)
 
 	minuendRange := Timerange{range1Start, range1End}
 	subtrahendRange := Timerange{range2Start, range2End}
@@ -31,8 +31,8 @@ func TestMinusPartialIntersection(t *testing.T) {
 
 	result := SubtractTimeranges(minuendRanges, subtrahendRanges)
 
-	expectedRangeStart := getTimeFromTodayUsingHoursMinutesSeconds(9, 0, 0)
-	expectedRangeEnd := getTimeFromTodayUsingHoursMinutesSeconds(11, 30, 0)
+	expectedRangeStart := getTimeFromTodayUsingHoursMinutesSeconds(9, 30, 0)
+	expectedRangeEnd := getTimeFromTodayUsingHoursMinutesSeconds(10, 0, 0)
 
 	expectedRange := Timerange{expectedRangeStart, expectedRangeEnd}
 
@@ -47,22 +47,144 @@ func TestMinusPartialIntersection(t *testing.T) {
 
 func TestMinusSameRange(t *testing.T) {
 
-	t.Errorf("Not implemented")
+	range1Start := getTimeFromTodayUsingHoursMinutesSeconds(9, 0, 0)
+	range1End := getTimeFromTodayUsingHoursMinutesSeconds(10, 0, 0)
+	range2Start := getTimeFromTodayUsingHoursMinutesSeconds(9, 0, 0)
+	range2End := getTimeFromTodayUsingHoursMinutesSeconds(10, 0, 0)
+
+	minuendRange := Timerange{range1Start, range1End}
+	subtrahendRange := Timerange{range2Start, range2End}
+
+	var minuendRanges []Timerange
+	var subtrahendRanges []Timerange
+
+	minuendRanges = append(minuendRanges, minuendRange)
+	subtrahendRanges = append(subtrahendRanges, subtrahendRange)
+
+	result := SubtractTimeranges(minuendRanges, subtrahendRanges)
+
+
+	var expectedRanges []Timerange
+
+	if !reflect.DeepEqual(expectedRanges, result) {
+		t.Errorf("Expected: %s, Actual: %s", timerangesToString(expectedRanges), timerangesToString(result))
+	}
 }
 
 func TestMinusNonIntersectingRange(t *testing.T) {
 
-	t.Errorf("Not implemented")
+	range1Start := getTimeFromTodayUsingHoursMinutesSeconds(9, 0, 0)
+	range1End := getTimeFromTodayUsingHoursMinutesSeconds(9, 30, 0)
+	range2Start := getTimeFromTodayUsingHoursMinutesSeconds(9, 30, 0)
+	range2End := getTimeFromTodayUsingHoursMinutesSeconds(15, 0, 0)
+
+	minuendRange := Timerange{range1Start, range1End}
+	subtrahendRange := Timerange{range2Start, range2End}
+
+	var minuendRanges []Timerange
+	var subtrahendRanges []Timerange
+
+	minuendRanges = append(minuendRanges, minuendRange)
+	subtrahendRanges = append(subtrahendRanges, subtrahendRange)
+
+	result := SubtractTimeranges(minuendRanges, subtrahendRanges)
+
+	expectedRangeStart := getTimeFromTodayUsingHoursMinutesSeconds(9, 0, 0)
+	expectedRangeEnd := getTimeFromTodayUsingHoursMinutesSeconds(9, 30, 0)
+
+	expectedRange := Timerange{expectedRangeStart, expectedRangeEnd}
+
+	var expectedRanges []Timerange
+
+	expectedRanges = append(expectedRanges, expectedRange)
+
+	if !reflect.DeepEqual(expectedRanges, result) {
+		t.Errorf("Expected: %s, Actual: %s", timerangesToString(expectedRanges), timerangesToString(result))
+	}
 }
 
 func TestManyRangesMinusOneRange(t *testing.T) {
 
-	t.Errorf("Not implemented")
+	minuend1Start := getTimeFromTodayUsingHoursMinutesSeconds(9, 0, 0)
+	minuend1End := getTimeFromTodayUsingHoursMinutesSeconds(9, 30, 0)
+	minuend2Start := getTimeFromTodayUsingHoursMinutesSeconds(10, 0, 0)
+	minuend2End := getTimeFromTodayUsingHoursMinutesSeconds(10, 30, 0)
+
+	subtrahendStart := getTimeFromTodayUsingHoursMinutesSeconds(9, 15, 0)
+	subtrahendEnd := getTimeFromTodayUsingHoursMinutesSeconds(10, 15, 0)
+
+	minuend1Range := Timerange{minuend1Start, minuend1End}
+	minuend2Range := Timerange{minuend2Start, minuend2End}
+	subtrahendRange := Timerange{subtrahendStart, subtrahendEnd}
+
+	var minuendRanges []Timerange
+	var subtrahendRanges []Timerange
+
+	minuendRanges = append(minuendRanges, minuend1Range, minuend2Range)
+	subtrahendRanges = append(subtrahendRanges, subtrahendRange)
+
+	result := SubtractTimeranges(minuendRanges, subtrahendRanges)
+
+	expectedRange1Start := getTimeFromTodayUsingHoursMinutesSeconds(9, 0, 0)
+	expectedRange1End := getTimeFromTodayUsingHoursMinutesSeconds(9, 15, 0)
+	expectedRange2Start := getTimeFromTodayUsingHoursMinutesSeconds(10, 15, 0)
+	expectedRange2End := getTimeFromTodayUsingHoursMinutesSeconds(10, 30, 0)
+
+	expected1Range := Timerange{expectedRange1Start, expectedRange1End}
+	expected2Range := Timerange{expectedRange2Start, expectedRange2End}
+
+	var expectedRanges []Timerange
+
+	expectedRanges = append(expectedRanges, expected1Range, expected2Range)
+
+	if !reflect.DeepEqual(expectedRanges, result) {
+		t.Errorf("Expected: %s, Actual: %s", timerangesToString(expectedRanges), timerangesToString(result))
+	}
 }
 
 func TestManyRangesMinusManyRanges(t *testing.T) {
 
-	t.Errorf("Not implemented")
+	minuend1Start := getTimeFromTodayUsingHoursMinutesSeconds(9, 0, 0)
+	minuend1End := getTimeFromTodayUsingHoursMinutesSeconds(11, 0, 0)
+	minuend2Start := getTimeFromTodayUsingHoursMinutesSeconds(13, 0, 0)
+	minuend2End := getTimeFromTodayUsingHoursMinutesSeconds(15, 0, 0)
+
+	subtrahend1Start := getTimeFromTodayUsingHoursMinutesSeconds(9, 0, 0)
+	subtrahend1End := getTimeFromTodayUsingHoursMinutesSeconds(9, 15, 0)
+	subtrahend2Start := getTimeFromTodayUsingHoursMinutesSeconds(10, 0, 0)
+	subtrahend2End := getTimeFromTodayUsingHoursMinutesSeconds(10, 15, 0)
+	subtrahend3Start := getTimeFromTodayUsingHoursMinutesSeconds(12, 30, 0)
+	subtrahend3End := getTimeFromTodayUsingHoursMinutesSeconds(16, 0, 0)
+
+	minuend1Range := Timerange{minuend1Start, minuend1End}
+	minuend2Range := Timerange{minuend2Start, minuend2End}
+	subtrahend1Range := Timerange{subtrahend1Start, subtrahend1End}
+	subtrahend2Range := Timerange{subtrahend2Start, subtrahend2End}
+	subtrahend3Range := Timerange{subtrahend3Start, subtrahend3End}
+
+	var minuendRanges []Timerange
+	var subtrahendRanges []Timerange
+
+	minuendRanges = append(minuendRanges, minuend1Range, minuend2Range)
+	subtrahendRanges = append(subtrahendRanges, subtrahend1Range, subtrahend2Range, subtrahend3Range)
+
+	result := SubtractTimeranges(minuendRanges, subtrahendRanges)
+
+	expectedRange1Start := getTimeFromTodayUsingHoursMinutesSeconds(9, 15, 0)
+	expectedRange1End := getTimeFromTodayUsingHoursMinutesSeconds(10, 0, 0)
+	expectedRange2Start := getTimeFromTodayUsingHoursMinutesSeconds(10, 15, 0)
+	expectedRange2End := getTimeFromTodayUsingHoursMinutesSeconds(11, 0, 0)
+
+	expected1Range := Timerange{expectedRange1Start, expectedRange1End}
+	expected2Range := Timerange{expectedRange2Start, expectedRange2End}
+
+	var expectedRanges []Timerange
+
+	expectedRanges = append(expectedRanges, expected1Range, expected2Range)
+
+	if !reflect.DeepEqual(expectedRanges, result) {
+		t.Errorf("Expected: %s, Actual: %s", timerangesToString(expectedRanges), timerangesToString(result))
+	}
 }
 
 func getTimeFromTodayUsingHoursMinutesSeconds(hours int, minutes int, seconds int) time.Time {
